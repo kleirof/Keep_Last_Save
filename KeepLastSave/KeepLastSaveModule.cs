6f70f06101f7e17f4120e02633a51c39240c3adf
kleirof <95400614+kleirof@users.noreply.github.com>
2024-12-17 19:34:43 +0800

    Add files via upload

---
+++ KeepLastSave/KeepLastSaveModule.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace KeepLastSave
+{
+	[BepInDependency("etgmodding.etg.mtgapi")]
+	[BepInPlugin(GUID, NAME, VERSION)]
+	public class KeepLastSaveModule : BaseUnityPlugin
+	{
+		public const string GUID = "kleirof.etg.keeplastsave";
+		public const string NAME = "Keep Last Save";
+		public const string VERSION = "1.0.1";
+		public const string TEXT_COLOR = "#FFCCCC";
+
+		public void Start()
+		{
+			ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
+		}
+
+		public void GMStart(GameManager g)
+		{
+			Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+
+			Harmony harmony = new Harmony(GUID);
+			harmony.PatchAll();
+		}
+
+		public static void Log(string text, string color = "FFFFFF")
+		{
+			ETGModConsole.Log($"<color={color}>{text}</color>");
+		}
+	}
+}
---
+++ KeepLastSave/KeepLastSavePatches.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+
+namespace KeepLastSave
+{
+	public static class KeepLastSavePatches
+	{
+		public static void EmitCall<T>(this ILCursor iLCursor, string methodName, Type[] parameters = null, Type[] generics = null)
+		{
+			MethodInfo methodInfo = AccessTools.Method(typeof(T), methodName, parameters, generics);
+			iLCursor.Emit(OpCodes.Call, methodInfo);
+		}
+
+		[HarmonyPatch(typeof(GameManager), nameof(GameManager.VerifyAndLoadMidgameSave))]
+		public class VerifyAndLoadMidgameSavePatchClass
+		{
+			[HarmonyILManipulator]
+			public static void VerifyAndLoadMidgameSavePatch(ILContext ctx)
+			{
+				ILCursor crs = new ILCursor(ctx);
+
+				if (crs.TryGotoNext(MoveType.After,
+					x => x.MatchCallvirt("System.Collections.Generic.List`1<System.String>", "Contains")
+					))
+				{
+					crs.EmitCall<VerifyAndLoadMidgameSavePatchClass>(nameof(VerifyAndLoadMidgameSavePatchClass.VerifyAndLoadMidgameSavePatchCall));
+				}
+			}
+
+			private static bool VerifyAndLoadMidgameSavePatchCall(bool orig)
+			{
+				return false;
+			}
+		}
+
+		[HarmonyPatch(typeof(MainMenuFoyerController), nameof(MainMenuFoyerController.InitializeMainMenu))]
+		public class InitializeMainMenuPatchClass
+		{
+			[HarmonyILManipulator]
+			public static void InitializeMainMenuPatch(ILContext ctx)
+			{
+				ILCursor crs = new ILCursor(ctx);
+
+				if (crs.TryGotoNext(MoveType.Before,
+					x => x.MatchCall<GameManager>("HasValidMidgameSave")
+					))
+				{
+					crs.Emit(OpCodes.Ldarg_0);
+					crs.EmitCall<InitializeMainMenuPatchClass>(nameof(InitializeMainMenuPatchClass.InitializeMainMenuPatchCall));
+				}
+			}
+
+			private static void InitializeMainMenuPatchCall(MainMenuFoyerController self)
+			{
+				if (GameManager.HasValidMidgameSave())
+				{
+					self.ContinueGameButton.IsEnabled = true;
+					self.ContinueGameButton.IsVisible = true;
+				}
+				else
+				{
+					self.ContinueGameButton.IsEnabled = false;
+					self.ContinueGameButton.IsVisible = false;
+				}
+			}
+		}
+	}
+}
